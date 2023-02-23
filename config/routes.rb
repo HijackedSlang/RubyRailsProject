f941b7a51125d0fb59cad57986f5e6fb93f8e722
HijackedSlang <73313832+HijackedSlang@users.noreply.github.com>
2023-02-23 17:49:22 -0600

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+    devise_for :users, :skip => [:sessions]
+  as :user do
+      get 'signin' => 'devise/sessions#new', :as => :new_user_session
+      post 'signin' => 'devise/sessions#create', :as => :user_session
+      get 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+  resources :rooms
+  root to: "rooms#index"
+  get "home", to: 'pages#home'
+  get "about", to: 'pages#about'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
